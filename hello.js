35d83dc4ad3ad90c7482dbeb29c6015675cb706b
cmh <pogni811@naver.com>
2021-03-26 08:37:28 +0900

    0323 학습내용

--- hello.js
+++ hello.js
@@ -1,4 +1,8 @@
-/*console.log('Hello World...!');
-console.log('Hello World...!');
-console.log('Hello World...!');
-console.log('Hello World...!');*/
+let data = new Date();
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
