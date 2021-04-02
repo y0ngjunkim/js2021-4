5147b6297766a6c0bb73716927ee778a3692359f
cmh <pogni811@naver.com>
2021-04-02 13:02:13 +0900

    0330 수업내용

--- hello.js
+++ hello.js
@@ -6,3 +6,8 @@ console.log(date.getDay());
 console.log(date.getHours());
 console.log(date.getMinutes());
 console.log(date.getSeconds());
+
+if (date.getHours() < 12) {
+    console.log("오전");
+}
+console.log("오후");
